f109bf7af89125f996ef589f66db0ba97a5b4164
christinen22 <christine.elin.louise@gmail.com>
2023-02-08 11:39:49 +0100

    Schema for Posts

--- project_posts/database/migrations/2023_02_08_095631_create_posts_table.php
+++ project_posts/database/migrations/2023_02_08_095631_create_posts_table.php
@@ -16,6 +16,11 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('title');
+            $table->string('user');
+            $table->string('email');
+            $table->string('description');
+
         });
     }
 
