bff86d5cbc9bf10a4b30ff32c61457ab955f54e0
christinen22 <christine.elin.louise@gmail.com>
2023-02-09 13:03:17 +0100

    Added navbar for upcoming Register & Login

--- project_posts/resources/views/layout.blade.php
+++ project_posts/resources/views/layout.blade.php
@@ -30,8 +30,19 @@
     <body class="mb-48">
 
     {{-- VIEW OUTPUT --}}
-
+    <nav class="flex justify-between items-center mb-4">
+        <a href="/"><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo" /></a>
+        <ul class="flex space-x-6 mr-6 text-lg">
+          <li>
+            <a href="/register" class="hover:text-laravel"><i class="fa-solid fa-user-plus"></i> Register</a>
+          </li>
+          <li>
+            <a href="/login" class="hover:text-laravel"><i class="fa-solid fa-arrow-right-to-bracket"></i> Login</a>
+          </li>
+        </ul>
+      </nav>
     <main>
+
     @yield('content')
     </main>
     <footer
