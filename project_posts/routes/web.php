6742fe0b0256216d223aab7831c7bbd20fe2f0e7
christinen22 <christine.elin.louise@gmail.com>
2023-02-09 13:03:01 +0100

    Added navbar for upcoming Register & Login

--- project_posts/routes/web.php
+++ project_posts/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,3 +38,6 @@ Route::delete('/posts/{post}', [PostController::class, 'destroy']);
 
 // Single Listing
 Route::get('/posts/{post}', [PostController::class, 'show']);
+
+//Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
